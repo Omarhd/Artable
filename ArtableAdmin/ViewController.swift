432667d8bb54dff30ff28faf3ace58b4d7fd30d2
Omar HD <omeralhadu@gmail.com>
2019-11-08 13:04:31 +0200

    Intial commit

---
+++ ArtableAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtableAdmin
+//
+//  Created by Omar on 08/11/2019.
+//  Copyright © 2019 Omar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
